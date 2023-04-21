46b4fececf01460ff08fde1189048125ed1559d1
Sergey Vozika <metasoma@yandex.ru>
2023-04-21 14:44:23 +0300

    Code Data Through API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
-type Data = {
-  name: string;
-  rock: string;
-};
+import code_data from "../../data/code_data";
+import type { Code } from "../../data/code_data";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Code[]>
 ) {
-  res.status(200).json({ name: "John Doe", rock: "Boryt Stoopoid" });
+  // res.status(200).json({ name: "John Doe", rock: "Boryt Stoopoid" });
+  res.status(200).json(code_data);
 }
